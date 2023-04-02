ed31411f23683f264a5e1bbcc85627883e3c4f76
fabioreiss <fabio83bass@gmail.com>
2023-04-02 18:33:09 -0300

    first commit

---
+++ Primeiroprograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.modell.Gato;
+
+public class Primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("hello mundo" +" " +   (a+b));*/
+		
+
+	}
+
+}
+class Livros {
+	private String nome;
+	private String npag;
+}
